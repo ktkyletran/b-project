9a3e317eb267ca3fe11144e72c1235fbded3a07c
Vanity <ktkyletran@gmail.com>
2021-12-07 19:36:43 -0600

    removed unneeded components and added navigation router

--- App.js
+++ App.js
@@ -1,32 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import Home from './src/screens/Home';
-import Post from './src/components/Post';
-import SearchResults from './src/screens/SearchResults';
-import feed from './assets/data/feed';
-import Search from './src/screens/Search';
-import Guests from './src/screens/Guests';
-
-const post1 = feed[0];
-const post2 = feed[1];
-const post3 = feed[2];
-const post4 = feed[3];
+import Router from './src/navigation/Router';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <SafeAreaView>
-        {/* <Home /> */}
-        {/* <Post post={post1}/>
-        <Post post={post2}/>
-        <Post post={post3}/>
-        <Post post={post4}/> */}
-        {/* <SearchResults /> */}
-        {/* <Search /> */}
-        <Guests />
-      </SafeAreaView>
+      <Router />
     </>
   );
 }
