65653fbfd3c1342a553ec70681e8dae73806e1ad
Vanity <ktkyletran@gmail.com>
2021-12-13 20:16:18 -0600

    aws amplify setup

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import React from 'react';
 import Router from './src/navigation/Router';
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   return (
